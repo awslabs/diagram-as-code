d12ed9c07a531397311ad7d435a9ea93f2985275
Yuta Kitamura <kitayu@amazon.com>
2024-05-04 20:23:05 +0900

    support HTTP Etag caching on internal/cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -7,14 +7,64 @@ import (
 	"archive/zip"
 	"crypto/md5"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+func createFileWithDirectory(filePath string) (*os.File, error) {
+	err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot create directory(%s): %v", filepath.Dir(filePath), err)
+	}
+
+	out, err := os.Create(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot create file(%s): %v", filePath, err)
+	}
+	return out, nil
+}
+
+func loadEtagCache(etagFilePath string) (string, error) {
+	// Check Etag file
+	if _, err := os.Stat(etagFilePath); err == nil {
+		f, err := os.Open(etagFilePath)
+		if err != nil {
+			return "", fmt.Errorf("Cannot open Etag file(%s): %v", etagFilePath, err)
+		}
+		defer f.Close()
+
+		bytes, err := ioutil.ReadAll(f)
+		if err != nil {
+			return "", fmt.Errorf("Cannot read Etag file(%s): %v", etagFilePath, err)
+		}
+		return string(bytes), nil
+	}
+	return "", nil
+}
+
+func writeEtagCache(etagFilePath, etag_value string) error {
+	out, err := createFileWithDirectory(etagFilePath)
+	if err != nil {
+		return fmt.Errorf("Cannot create file with directory: %v", err)
+	}
+	defer out.Close()
+
+	d := []byte(etag_value)
+	_, err = out.Write(d)
+	if err != nil {
+		return fmt.Errorf("Cannot write Etag file(%s): %v", etagFilePath, err)
+	}
+	return nil
+}
+
 func FetchFile(url string) (string, error) {
+	log.WithField("prefix", "[internal/cache/cache.go]")
+	log.Infof("[internal/cache/cache.go] FetchFile %s", url)
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		return "", fmt.Errorf("Cannot get home directory: %v", err)
@@ -22,29 +72,63 @@ func FetchFile(url string) (string, error) {
 
 	hashedUrl := md5.New()
 	io.WriteString(hashedUrl, url)
+
+	etagFilePath := filepath.Join(homeDir, ".cache", "awsdac", "etag", fmt.Sprintf("%x-%s", hashedUrl.Sum(nil), filepath.Base(url)))
 	cacheFilePath := filepath.Join(homeDir, ".cache", "awsdac", fmt.Sprintf("%x-%s", hashedUrl.Sum(nil), filepath.Base(url)))
 
+	cached_etag_value := ""
 	// Check cached same URL resource
-	if _, err := os.Stat(cacheFilePath); err != nil {
-
-		resp, err := http.Get(url)
+	if _, err := os.Stat(cacheFilePath); err == nil {
+		cached_etag_value, err = loadEtagCache(etagFilePath)
 		if err != nil {
-			return "", fmt.Errorf("Cannot get HTTP resource(%s): %v", url, err)
+			return "", fmt.Errorf("Cannot load Etag Cache: %v", err)
 		}
-		defer resp.Body.Close()
+	}
 
-		if resp.StatusCode < 200 || resp.StatusCode >= 400 {
-			return "", fmt.Errorf("Failed to fetch file %s: http status %d", url, resp.StatusCode)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("Cannot create HTTP request(%s): %v", url, err)
+	}
+
+	if cached_etag_value != "" {
+		log.Infof("[internal/cache/cache.go] Found previous Etag cache. Use HTTP Etag value %s", cached_etag_value)
+		req.Header.Add("If-None-Match", cached_etag_value)
+	}
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("Cannot get HTTP resource(%s): %v", url, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		return "", fmt.Errorf("Failed to fetch file %s: http status %d", url, resp.StatusCode)
+	}
+
+	etag_value := ""
+	if etag, ok := resp.Header["Etag"]; ok && len(etag) > 0 {
+		z := strings.SplitN(etag[0], "/", 2)
+		etag_value = z[0]
+		if z[0] == "W" { // weak validator was returned.
+			etag_value = z[1]
 		}
+	}
+	log.Infof("[internal/cache/cache.go] Server respond with HTTP %d", resp.StatusCode)
 
-		err = os.MkdirAll(filepath.Dir(cacheFilePath), os.ModePerm)
+	if resp.StatusCode == 302 && cached_etag_value == "" {
+		return "", fmt.Errorf("Remote server is responding with an HTTP 304 even though no If-none-match header was added to the request")
+	}
+
+	if resp.StatusCode == 302 && cached_etag_value != etag_value {
+		return "", fmt.Errorf("Remote server is responding with an HTTP 304 even though mismatch between Etag response header and If-none-Match request header")
+	}
+
+	// save remote resource to local if no local cache or etag mismatch or server doesn't send etag
+	if cached_etag_value == "" || etag_value == "" || cached_etag_value != etag_value {
+		out, err := createFileWithDirectory(cacheFilePath)
 		if err != nil {
-			return "", fmt.Errorf("Cannot create directory(%s): %v", filepath.Dir(cacheFilePath), err)
-		}
-
-		out, err := os.Create(cacheFilePath)
-		if err != nil {
-			return "", fmt.Errorf("Cannot create file(%s): %v", cacheFilePath, err)
+			return "", fmt.Errorf("Cannot create file with directory: %v", err)
 		}
 		defer out.Close()
 
@@ -52,6 +136,17 @@ func FetchFile(url string) (string, error) {
 		if err != nil {
 			return "", fmt.Errorf("Cannot copy: %v", err)
 		}
+
+		// save as Etag
+		if etag_value != "" {
+			log.Infof("[internal/cache/cache.go] Server respond with Etag. Save Etag value %s", etag_value)
+			err := writeEtagCache(etagFilePath, etag_value)
+			if err != nil {
+				return "", fmt.Errorf("Cannot write Etag cache(%s): %v", etagFilePath, err)
+			}
+		}
+	} else {
+		log.Infof("[internal/cache/cache.go] Use cache based on matched HTTP Etag")
 	}
 	return cacheFilePath, nil
 }
@@ -87,24 +182,20 @@ func ExtractZipFile(filePath string) (string, error) {
 			if err != nil {
 				return "", fmt.Errorf("Cannot open file(%s): %v", f.Name, err)
 			}
+			defer rc.Close()
 
 			outputFilename := fmt.Sprintf("%s/%s", cacheFilePath, f.Name)
 
-			err = os.MkdirAll(filepath.Dir(outputFilename), os.ModePerm)
+			fo, err := createFileWithDirectory(outputFilename)
 			if err != nil {
-				return "", fmt.Errorf("Cannot create directory(%s): %v", filepath.Dir(outputFilename), err)
+				return "", fmt.Errorf("Cannot create file with directory: %v", err)
 			}
+			defer fo.Close()
 
-			fo, err := os.Create(outputFilename)
-			if err != nil {
-				return "", fmt.Errorf("Cannot create file(%s): %v", outputFilename, err)
-			}
 			_, err = io.Copy(fo, rc)
 			if err != nil {
 				return "", fmt.Errorf("Cannot copy: %v", err)
 			}
-			rc.Close()
-			fo.Close()
 		}
 
 		defer r.Close()
