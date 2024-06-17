f00b333a07d7fb388c44e77cbbae21a532824068
Yuta Kitamura <kitayu@amazon.com>
2024-06-17 15:32:40 +0900

    add: liberation font paths for major linux distributions

--- internal/font/linux.go
+++ internal/font/linux.go
@@ -7,6 +7,9 @@
 package font
 
 var Paths = []string{
-	"/usr/share/fonts/truetype/msttcorefonts/Arial.ttf",
-	"/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf",
+	"/usr/share/fonts/truetype/msttcorefonts/Arial.ttf",                // For Ubuntu linux ttf-mscorefonts-installer package.
+	"/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",  // For Ubuntu/Debian Linux fonts-liberation package.
+	"/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf", // For Ubuntu/Debian Linux fonts-liberation2 package.
+	"/usr/share/fonts/liberation-sans/LiberationSans-Regular.ttf",      // For Fedora/AL2023 Linux liberation-sans-fonts package.
+	"/usr/share/fonts/liberation/LiberationSans-Regular.ttf",           // For Alpine/Arch Linux ttf-liberation package.
 }
