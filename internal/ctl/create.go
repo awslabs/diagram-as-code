77ba33daf35d266ac7f99af2641b938af6a46818
Yuta Kitamura <kitayu@amazon.com>
2025-03-22 23:06:01 +0900

    remove AWS::Diagram::TextBox

--- internal/ctl/create.go
+++ internal/ctl/create.go
@@ -164,8 +164,6 @@ func loadResources(template *TemplateStruct, ds definition.DefinitionStructure, 
 			resources[k] = new(types.VerticalStack).Init()
 		case "AWS::Diagram::HorizontalStack":
 			resources[k] = new(types.HorizontalStack).Init()
-		case "AWS::Diagram::TextBox":
-			resources[k] = new(types.Resource).Init()
 		default:
 			def, ok := ds.Definitions[v.Type]
 			if !ok {
